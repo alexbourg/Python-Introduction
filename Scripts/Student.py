84556458dbf5bb0760ae7f330d5806296fb236bb
alexbourg <51832082+alexbourg@users.noreply.github.com>
2020-03-31 00:05:47 +0200

    Scripts

--- Scripts/Student.py
+++ Scripts/Student.py
@@ -4,5 +4,7 @@ from Student_Class import Student
 student1 = Student("Alex", "CyberSecuirty", 3.5, True)
 student2 = Student("Bonbon", "Strategy", 4, False)
 
-print(student1.is_on_internship)
-print(student2.is_on_internship)
+print(student1.name, student1.major, student1.gpa, student1.is_on_internship)
+# print(student2.is_on_internship)
+
+print(student1.on_honor_roll())
