7bb24f408ea31acddb631ea522255f84d8b82430
alexbourg <51832082+alexbourg@users.noreply.github.com>
2020-03-31 00:00:14 +0200

    Scripts

---
+++ Scripts/Student.py
@@ -0,0 +1,8 @@
+from Student_Class import Student
+
+
+student1 = Student("Alex", "CyberSecuirty", 3.5, True)
+student2 = Student("Bonbon", "Strategy", 4, False)
+
+print(student1.is_on_internship)
+print(student2.is_on_internship)
